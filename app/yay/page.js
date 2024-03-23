2cb88457ddbafe5be5b2389654dbf205a39b5e9a
deltabladex <121876767+deltabladex@users.noreply.github.com>
2024-03-23 17:24:04 +1100

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,7 +3,7 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'Yayyy~ lets plan a date <3'}
+      {'Yayyy~ <3'}
     </div>
   )
 
